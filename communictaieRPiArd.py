4e93a411f1bbdbf76a3ac9975462be10d4b545b1
JohanColson <83575554+JohanColson@users.noreply.github.com>
2022-06-11 11:24:03 +0200

    Update communictaieRPiArd.py

--- communictaieRPiArd.py
+++ communictaieRPiArd.py
@@ -1,28 +1,10 @@
-from nntplib import GroupInfo
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# lsusb to check device name
-#dmesg | grep "tty" to find port name
-
-import serial,time
-
-
+import serial
+import time
 if __name__ == '__main__':
-    
-    print('Running. Press CTRL-C to exit.')
-    with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
-        time.sleep(0.1) #wait for serial to open
-        if arduino.isOpen():
-            print("{} connected!".format(arduino.port))
-            try:
-                while True:
-                    cmd=input("Enter command : ")
-                    arduino.write(cmd.encode())
-                    #time.sleep(0.1) #wait for arduino to answer
-                    while arduino.inWaiting()==0: pass
-                    if  arduino.inWaiting()>0: 
-                        answer=arduino.readline()
-                        print(answer)
-                        arduino.flushInput() #remove data after reading
-            except KeyboardInterrupt:
-                print("KeyboardInterrupt has been caught.")
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+    while True:
+        ser.write(b"Hello from Raspberry Pi!\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        time.sleep(1)
